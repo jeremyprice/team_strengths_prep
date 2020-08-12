1e66cd938171469912e886cc2d7c65b00ec6d5d8
Jeremy Price <jprice@alumni.rose-hulman.edu>
2020-08-12 07:09:05 -0500

    added code to look for the caution page

--- process_34report.py
+++ process_34report.py
@@ -16,8 +16,7 @@ def process(r34_fname, r34_dir, p21_dir):
     '''
     split_and_extract(r34_fname, r34_dir, p21_dir)
 
-def find_name(doc, page_num, no_spaces=True):
-    page = doc[page_num]
+def find_name(page, no_spaces=True):
     blocks = page.getText('blocks')
     # the name block looks like this: FIRST LAST | DATE
     name_blocks = [b for b in blocks if ' | ' in b[4]]
@@ -31,6 +30,13 @@ def find_name(doc, page_num, no_spaces=True):
     if no_spaces:
         name = name.replace(' ', '')
     return name
+
+def check_for_caution(page):
+    blocks = page.getText('blocks')
+    # the first page of the report contains A NOTE OF CAUTION
+    name_blocks = [b for b in blocks if 'A NOTE OF CAUTION' in b[4]]
+    # if there is a block with this text, we have a caution page
+    return len(name_blocks) == 1
 
 def split_and_extract(fname, r34_outdir, p21_outdir):
     '''Split a Gallup full 34 report into the individual files
@@ -48,15 +54,19 @@ def split_and_extract(fname, r34_outdir, p21_outdir):
     for report_num in range(report_count):
         start_page_num = (report_num * 26)
         stop_page_num = start_page_num + 25  # we only need the first 25 pages since the last is blank
-        person_name = find_name(doc, start_page_num)
+        p21 = 20
+        person_name = find_name(doc[start_page_num])
         print(person_name)
+        if check_for_caution(doc[start_page_num]):
+            stop_page_num += 1
+            p21 += 1
         r34_fname = fname_34fmt.format(person_name)
         p21_fname = fname_21fmt.format(person_name)
         print("Page {}: {}".format(start_page_num, report_num))
         doc.select(range(start_page_num, stop_page_num))
-        doc.save(r34_fname)
-        doc.select([20])
-        doc.save(p21_fname)
+        doc.save(r34_fname, garbage=4, clean=1, deflate=1)
+        doc.select([p21])
+        doc.save(p21_fname, garbage=4, clean=1, deflate=1)
         doc.close()
         doc = fitz.open(fname)
 
